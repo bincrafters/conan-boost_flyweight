e4facaf8b82cc21d48c2c95157a00eb0c37f50b6
Jerry W <jerrywiltse@gmail.com>
2018-01-25 01:12:51 -0500

    Refresh round 2

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,8 @@ class BoostFlyweightConan(ConanFile):
     name = "boost_flyweight"
     version = "1.66.0"
     url = "https://github.com/bincrafters/conan-boost_flyweight"
-
+    author = "Bincrafters <bincrafters@gmail.com>"
+    exports = ["LICENSE.md"]
     lib_short_names = ["flyweight"]
     is_header_only = True
 
