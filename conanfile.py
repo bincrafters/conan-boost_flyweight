dee1e34720794db8cfc2682bf7577d86616acec6
Rene Rivera <grafikrobot@gmail.com>
2018-11-29 22:01:12 -0600

    Initial 1.68.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.67.0@bincrafters/testing")
+base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostFlyweightConan(base.BoostBaseConan):
     name = "boost_flyweight"
@@ -27,5 +27,3 @@ class BoostFlyweightConan(base.BoostBaseConan):
         "boost_throw_exception",
         "boost_type_traits"
     ]
-
-
